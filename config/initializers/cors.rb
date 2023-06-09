103335707bf83b91e64fc9d7c49252204778f74d
António Fernandes <antoniofernandes@MBP-de-Antonio.Home>
2023-06-09 10:55:43 +0100

    update final version

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3002', 'https://deluxe-daifuku-66f877.netlify.app, https://antoniofernandes.com'
+      origins 'http://localhost:3002', 'https://deluxe-daifuku-66f877.netlify.app', 'https://antoniofernandes.com'
       resource '*', headers: :any, methods: [:get, :post]
     end
   end
