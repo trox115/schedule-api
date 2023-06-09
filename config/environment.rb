1cc0a8c06b7091e2d2a9973135969af1ce4e2a9a
António Fernandes <antoniofernandes@MBP-de-Antonio.Home>
2023-06-09 11:51:15 +0100

    update sendgrid api

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
   :password => Rails.application.credentials[:sendgrid][:api_key], # This is the secret sendgrid API key which was issued during API key creation
-  :domain => 'https://rocky-fjord-82477.herokuapp.com/',
+  :domain => 'https://lively-grass-9664.fly.dev/',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
